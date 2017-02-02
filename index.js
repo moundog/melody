a23fe45a6d577f2af04a9de501ffc7bb12e7b10d
Mounir Krichane <mounir.krichane@rts.ch>
2017-02-02 15:13:30 +0100

    Changed repositotry name

--- index.js
+++ index.js
@@ -1,38 +1,51 @@
+// Copyright 2016, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the 'License');
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 'use strict';
 
+process.env.DEBUG = 'actions-on-google:*';
+let Assistant = require('actions-on-google').ApiAiAssistant;
 let express = require('express');
 let bodyParser = require('body-parser');
-let ApiAiAssistant = require('actions-on-google').ApiAiAssistant;
-let winston = require('winston');
-
-// Configuration
-const config = require('./config.js');
-winston.level = 'info';
 
 let app = express();
-app.set('port', (config.port || 8080));
 app.use(bodyParser.json({type: 'application/json'}));
 
-app.post('/', function (request, response) {
+// [START YourAction]
+app.post('/', function (req, res) {
+  const assistant = new Assistant({request: req, response: res});
+  console.log('Request headers: ' + JSON.stringify(req.headers));
+  console.log('Request body: ' + JSON.stringify(req.body));
 
-  winston.log('info', 'Processing request from API.AI');
-
-  const assistant = new ApiAiAssistant({request: request, response: response});
-
-  const FORECAST_INTENT = 'input.getForecast'; // the action name from the API.AI intent
-
-  function forecastIntent(assistant) {
-    assistant.tell('I will give you the weather forecast');
+  // Fulfill action business logic
+  function responseHandler (assistant) {
+    // Complete your fulfillment logic and send a response
+    let text_to_speech = '<speak><audio src="https://rtsww-a-d.rts.ch/couleur3/programmes/supersonic/2017/supersonic_20170126_standard_les-orties_bead29ea-9dca-465c-8d43-b2b4fbfc8f82-128k.mp3">I could not play the audio file</audio></speak>';
+    assistant.tell(text_to_speech);
   }
 
-  let actionMap = new Map();
-  actionMap.set(FORECAST_INTENT, forecastIntent);
-
-  assistant.handleRequest(actionMap);
-
+  assistant.handleRequest(responseHandler);
 });
+// [END YourAction]
 
-// Start the server
-let server = app.listen(app.get('port'), function () {
-  winston.log('info', 'Express listening on port ' + config.port);
-});
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  let server = app.listen(process.env.PORT || 8080, function () {
+    let port = server.address().port;
+    console.log('App listening on port %s', port);
+  });
+  // [END server]
+}
+
+module.exports = app;
